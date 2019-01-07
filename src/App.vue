6732878c8d6e1d32de61bd32d8ac4acd390951ec
Markus Oberlehner <markus.oberlehner@gmail.com>
2019-01-07 05:12:09 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="App o-container o-container--s o-vertical-spacing o-vertical-spacing--xl">
+    <h1>How to Use GraphQL with Vue Apollo Components and Netlify Functions</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: `App`,
+};
+</script>
+
+<style lang="scss">
+@import './assets/scss/settings/**/*';
+@import './assets/scss/generic/**/*';
+@import '{
+  .o-container,
+  .o-container--s,
+} from ~@avalanche/object-container';
+@import '{
+  .o-vertical-spacing,
+  .o-vertical-spacing--xl,
+} from ~@avalanche/object-vertical-spacing';
+
+.App {
+  padding-top: setting-spacing(xl);
+  padding-bottom: setting-spacing(xl);
+
+  h1 {
+    text-align: center;
+  }
+}
+</style>
