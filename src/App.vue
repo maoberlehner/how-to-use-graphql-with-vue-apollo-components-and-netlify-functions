02a40784516ffe9195e59314904c275e6be249f8
Markus Oberlehner <markus.oberlehner@gmail.com>
2019-01-19 07:42:46 +0100

    Render list of articles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,46 @@
 <template>
   <div class="App o-container o-container--s o-vertical-spacing o-vertical-spacing--xl">
     <h1>How to Use GraphQL with Vue Apollo Components and Netlify Functions</h1>
+    <ApolloQuery
+      :query="require('./queries/articles.gql')"
+    >
+      <template slot-scope="{ result: { loading, error, data } }">
+        <div v-if="loading">
+          Loading...
+        </div>
+        <div v-else-if="error">
+          An error occured! Please try again.
+        </div>
+        <ul
+          v-else-if="data"
+          class="App__article-list"
+        >
+          <li
+            v-for="article in data.articles"
+            :key="article.id"
+            class="App__article-list-item"
+          >
+            <div class="App__article-list-figure">
+              <img
+                :src="article.image"
+                alt=""
+              >
+            </div>
+            <div class="App__article-list-body">
+              <h2>
+                {{ article.title }}
+              </h2>
+              <p>
+                {{ article.body }}
+              </p>
+            </div>
+          </li>
+        </ul>
+        <div v-else>
+          There are no articles yet. Please come back later.
+        </div>
+      </template>
+    </ApolloQuery>
   </div>
 </template>
 
@@ -29,5 +69,19 @@ export default {
   h1 {
     text-align: center;
   }
+}
+
+.App__article-list-item {
+  display: flex;
+
+  &:not(:first-child) {
+    margin-top: setting-spacing(l);
+    padding-top: setting-spacing(l);
+    border-top: 1px solid #e2e2e2;
+  }
+}
+
+.App__article-list-body {
+  margin-left: setting-spacing(m);
 }
 </style>
