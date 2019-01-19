5f7111e59e7ca6c53eea87b970f82b4bf213483f
Markus Oberlehner <markus.oberlehner@gmail.com>
2019-01-19 07:27:59 +0100

    Add graphql endpoint

--- src-functions/graphql.js
+++ src-functions/graphql.js
@@ -1,1 +1,30 @@
-exports.handler = () => {};
+
+import { ApolloServer, gql } from 'apollo-server-lambda';
+
+import articles from '../data/articles.json';
+
+const typeDefs = gql`
+  type Article {
+    body: String
+    id: Int
+    image: String
+    title: String
+  }
+
+  type Query {
+    articles: [Article]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    articles: () => articles,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+exports.handler = server.createHandler();
